c6dc0d9f42a00f7f54df3933944c8d2cccfae2b1
김소희 <sohui0103@naver.com>
2024-05-09 01:04:56 +0900

    static

--- appPaas/settings.py
+++ appPaas/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7*$8pwo-(f0nbpb#++*!%x8g=r5*4k*@5(@fp1*wzi&nieq@-z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Application definition
